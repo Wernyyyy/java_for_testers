7b8852fc8e8a68c62e867ecb6e317a8c44856cb4
Sergey <wernyyyy@gmail.com>
2025-04-10 00:02:25 +0300

    Создание теста SquareTests

--- sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
@@ -3,11 +3,9 @@ package ru.stqa.geometry.figures;
 
 public class Square {
     public static void printSquareArea(double side) {
-        var text = (String.format("Площадь квадрата со стороной %f = %f", side, squareArea(side)));
+        String text = String.format("Площадь квадрата со стороной %f = %f", side, squareArea(side));
         System.out.println(text);
     }
 
-    private static double squareArea(double a) {
-        return a * a;
-    }
+    public static double squareArea(double a) { return a * a; }
 }
---
+++ sandbox/src/test/java/ru/stqa/geometry/figures/SquareTests.java
@@ -0,0 +1,14 @@
+package ru.stqa.geometry.figures;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SquareTests {
+
+    @Test
+    void canCalculateArea() {
+        double result = Square.squareArea(6.0);
+        Assertions.assertEquals(25.0, result);
+
+    }
+}
