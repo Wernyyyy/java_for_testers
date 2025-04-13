3446641d73f2ab9b6986af00eddb7859fe67c266
Sergey <wernyyyy@gmail.com>
2025-04-14 01:22:31 +0300

    Добавление объектов

--- sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
@@ -2,10 +2,27 @@ package ru.stqa.geometry.figures;
 
 
 public class Square {
-    public static void printSquareArea(double side) {
-        String text = String.format("Площадь квадрата со стороной %f = %f", side, squareArea(side));
+
+    double side; //Свойство объекта
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    public static void printSquareArea(Square s) {
+        String text = String.format("Площадь квадрата со стороной %f = %f", s.side, s.area());
         System.out.println(text);
     }
 
-    public static double squareArea(double a) { return a * a; }
+
+
+
+
+    public double area() {
+        return this.side * this.side;
+    }
+
+    public double perimeter() {
+         return 4 * this.side;
+    }
 }
--- sandbox/src/test/java/ru/stqa/geometry/figures/SquareTests.java
+++ sandbox/src/test/java/ru/stqa/geometry/figures/SquareTests.java
@@ -7,8 +7,14 @@ public class SquareTests {
 
     @Test
     void canCalculateArea() {
-        double result = Square.squareArea(6.0);
+        var s = new Square(5.0);
+        double result = s.area();
         Assertions.assertEquals(25.0, result);
 
+    }
+
+    @Test
+    void canCalculatePerimeter() {
+       Assertions.assertEquals(20.0, new Square(5.0).perimeter());
     }
 }
